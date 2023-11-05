e3863e2c1d2944ddc1fb671e29175662fe5696c8
Hugh <Hugh@37ChapelLane.co.uk>
2023-11-05 05:16:41 +0000

    initial revision

---
+++ AutofillFromCSV/Program.cs
@@ -0,0 +1,19 @@
+﻿using Autofills.Common;
+using System.Runtime.CompilerServices;
+
+namespace Autofills.AutofillFromCSV
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Use default file name or take from the commandline
+            string inputFile = args.Length > 0 ? args[0] : @".\input.csv";
+
+            string outputFile = args.Length > 1 ? args[1] : @"h:\Autofills\autofill.txt";
+
+            if(BusWankers.CheckPaths(inputFile, outputFile))
+                BusWankers.GenerateAutofillText(inputFile, outputFile);
+        }
+    }
+}
