900a8332d7dd3fdc2addce9482bc7b4e4dc46fa0
Hugh Jones <Hugh@37ChapelLane.co.uk>
2023-11-13 17:01:17 +0000

    touch

--- UploaderService/Controllers/UploadServiceController.cs
+++ UploaderService/Controllers/UploadServiceController.cs
@@ -1,36 +1,41 @@
 ﻿using Autofills.Common;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Net;
-using System.Net.Mail;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace UploaderService.Controllers
+[ApiController]
+[Route("[controller]")]
+public class UploadServiceController : ControllerBase
 {
-    [Route("BusWankers")]
-    [ApiController]
-    [AllowAnonymous]
-
-    public class UploadServiceController : ControllerBase
+    [HttpPost]
+    public async Task<IActionResult> Post(IFormFile file)
     {
-        [HttpPost]
-        public async void Upload()
+        if(file == null || file.Length == 0)
         {
-            await Task.Run(() =>
-            {
-                string inputFile = @".\Spreadsheet.xlsx";
-                string outputPath = @".\\Autofills";
-
-                if(BusWankers.CheckPaths(inputFile, outputPath + "\\dummytext.txt"))
-                    ExcelFileHelper.SaveAsCsv(inputFile, outputPath);
-
-                BusWankers.GenerateAutofillText($"{outputPath}\\Thursday - Coach.csv", $"{outputPath}\\bw_autofills.txt");
-                BusWankers.GenerateAutofillText($"{outputPath}\\Sunday - General.csv", $"{outputPath}\\g_autofills.txt");
-            });
+            return BadRequest("Upload a file.");
         }
+
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "uploads", file.FileName);
+
+        using(var stream = new FileStream(path, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        GenerateAutofills(path);
+
+        return Ok(new { file.FileName, file.Length });
     }
 
-}
+    private async void GenerateAutofills(string inputFile)
+    {
+        await Task.Run(() =>
+        {
+            string outputPath = @".\\Autofills";
+
+            if(BusWankers.CheckPaths(inputFile, outputPath + "\\dummytext.txt"))
+                ExcelFileHelper.SaveAsCsv(inputFile, outputPath);
+
+            BusWankers.GenerateAutofillText($"{outputPath}\\Thursday - Coach.csv", $"{outputPath}\\bw_autofills.txt");
+            BusWankers.GenerateAutofillText($"{outputPath}\\Sunday - General.csv", $"{outputPath}\\g_autofills.txt");
+        });
+    }
+} 
